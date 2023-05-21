840d8881dc071bc2b5743ab8b74258e982eaf496
Aayushi Saini <118258257+AayushiSaini@users.noreply.github.com>
2023-05-22 00:24:34 +0530

    Add files via upload

---
+++ SmsReceiver.java
@@ -0,0 +1,29 @@
+package com.example.messageapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+Bundle bundle=intent.getExtras();
+Object[] smsObj= (Object[]) bundle.get("pdus");
+for(Object obj:smsObj){
+            SmsMessage message=SmsMessage.createFromPdu((byte[]) obj);
+            String mobNo= message.getDisplayOriginatingAddress();
+            String msg=message.getDisplayMessageBody();
+            Log.d("MsgDetails","MobNo:"+mobNo+" ,Msg:"+msg);
+    SmsManager smsManager=SmsManager.getDefault();
+    smsManager.sendTextMessage("+917505349949",null,"hello",null,null);
+
+
+}
+
+
+    }
+}
